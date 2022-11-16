898c98c66e5502137b11dddd301af8768662df6a
Nina Brazhnikova <nbrazhnikova@gmail.com>
2022-11-16 14:28:37 +0300

    if_else added

---
+++ Example_005_if/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username == "Marusya")
+{
+    Console.WriteLine("Hurray, it's Marusya!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
+
+Console.Write("Enter username1: ");
+string username1 = Console.ReadLine();
+
+if(username1.ToLower() == "marusya")
+{
+    Console.WriteLine("Hurray, it's Marusya!!!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username1);
+}
