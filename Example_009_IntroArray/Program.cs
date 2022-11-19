670662758d968e756dd3c7ee72ea8f4332e2ae52
Nina Brazhnikova <nbrazhnikova@gmail.com>
2022-11-20 01:46:17 +0300

    Inro Array added

---
+++ Example_009_IntroArray/Program.cs
@@ -0,0 +1,17 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+int [] array = {155, 21, 39, 35, 211, 96, 51, 11, 19};
+
+int max = Max(
+    Max(array[0], array[1], array[2]),
+    Max(array[3], array[4], array[5]),
+    Max(array[6], array[7], array[8])
+);
+
+Console.WriteLine(max);
